1f2d7cb7a4d703cdc0c04371e87d5d4a7b800156
Crayonex <95239496+Crayonex@users.noreply.github.com>
2021-12-08 19:22:26 +0300

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,26 @@
+      function many_colours(colour) {
+        switch(colour) {
+          case('green'):return 'red'
+          case('blue'):return 'green'
+          case('red'):return 'blue'
+        }
+      }
+      console.log(many_colours(''));
+
+      go.onclick = changeColor;
+
+      function changeColor() {
+      console.log(color.value);
+      vasya.style = "color: " + color.value + ";";
+      }
+      for(let i=0; i<-10;i++){
+        if(i=0)continue;
+        console.log(i);
+      }
+
+      function sum() {
+        alert('ok');
+      }
+      console.log(sum(5,7));
+
+      vasya2.style = ();
